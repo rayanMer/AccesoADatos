7434314ea5def162d3d4ef428dc838d6cd8c1b5a
RayanMerimi <rayanoujda707@gmail.com>
2025-01-21 14:19:19 +0100

    21-01-2025 14:19

---
+++ hibernate-core/src/main/java/org/hibernate/orm/hibernate_core/hibernate-core/Application.java
@@ -0,0 +1,9 @@
+package org.hibernate.orm.hibernate_core.hibernate-core;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello hibernate-core");
+    }
+
+}
