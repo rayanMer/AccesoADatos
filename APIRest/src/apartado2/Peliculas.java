55fe4d6e96938b19f43eab65335c8257a60582e2
RayanMerimi <rayanoujda707@gmail.com>
2025-02-11 13:41:05 +0100

    11-02-2025 13:41

---
+++ APIRest/src/apartado2/Peliculas.java
@@ -0,0 +1,14 @@
+package apartado2;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Peliculas {
+	private String id;
+	private String nombre;
+	private String director;
+	private String clasificacion;
+	
+
+}
